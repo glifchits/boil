755a520625aa12c7908db04b41da444e8c77d230
George Lifchits <george.lifchits@gmail.com>
2016-01-10 11:53:07 -0500

    cleaned up the shell function

--- boil.sh
+++ boil.sh
@@ -4,23 +4,22 @@ function boil ()
   tmp="tmpzip"
   tmpzip="tmp.zip"
 
-  echo "Repo: $REPO"
-  #if [[ $REPO =~ .*\/.* ]] && echo "match" || echo "bad"a
-
   # download the repo master as zip file
   zipurl="https://github.com/$REPO/archive/master.zip"
-  curl $zipurl -L0ko $tmpzip
+  echo "Downloading $REPO:master"
+  curl $zipurl -L0ko $tmpzip &> /dev/null
 
   # unzip folder to tmp
-  unzip $tmpzip -d ./$tmp
+  echo "Unzipping..."
+  unzip $tmpzip -d ./$tmp &> /dev/null
 
   # get name of the internal folder
   folder=$(ls $tmp)
 
   for obj in $( ls "$tmp/$folder" ); do
-    #kjecho $obj
     cp -R "$tmp/$folder/$obj" .
   done
 
-  rm -rf tmpzip
+  echo "done. cleaning up"
+  rm -rf $tmpzip $tmp
 }
